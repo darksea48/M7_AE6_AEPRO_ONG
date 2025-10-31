ca781a08e8792489da3723a038575ea0f3f8f9fd
Douglas Suárez Zamorano <d.suarez.zamorano@gmail.com>
2025-10-30 21:24:56 -0300

    modificaciones en forms y views

--- gestor/views.py
+++ gestor/views.py
@@ -4,17 +4,17 @@ from gestor.forms import VoluntarioForm, EventoForm
 
 def lista_voluntarios(request):
     voluntarios = Voluntario.objects.all()
-    return render(request, 'lista_voluntarios.html', {'voluntarios': voluntarios})
+    return render(request, 'voluntarios_list.html', {'voluntarios': voluntarios})
 
 def crear_voluntario(request):
     if request.method == 'POST':
         form = VoluntarioForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_voluntarios')
+            return redirect('voluntarios_list')
     else:
         form = VoluntarioForm()
-    return render(request, 'form_voluntario.html', {'form': form})
+    return render(request, 'voluntario_form.html', {'form': form})
 
 def editar_voluntario(request, id):
     voluntario = get_object_or_404(Voluntario, id=id)
@@ -22,33 +22,33 @@ def editar_voluntario(request, id):
         form = VoluntarioForm(request.POST, instance=voluntario)
         if form.is_valid():
             form.save()
-            return redirect('lista_voluntarios')
+            return redirect('voluntarios_list')
     else:
         form = VoluntarioForm(instance=voluntario)
-    return render(request, 'voluntarios/form_voluntario.html', {'form': form})
+    return render(request, 'voluntario_form.html', {'form': form})
 
 def eliminar_voluntario(request, id):
     voluntario = get_object_or_404(Voluntario, id=id)
     if request.method == 'POST':
         voluntario.delete()
-        return redirect('lista_voluntarios')
-    return render(request, 'voluntarios/confirmar_eliminar.html', {'voluntario': voluntario})
+        return redirect('voluntarios_list')
+    return render(request, 'voluntario_confirm_delete.html', {'voluntario': voluntario})
 
 # eventos.
 
 def lista_eventos(request):
     eventos = Evento.objects.all()
-    return render(request, 'lista_eventos.html', {'eventos': eventos})
+    return render(request, 'eventos_list.html', {'eventos': eventos})
 
 def crear_evento(request):
     if request.method == 'POST':
         form = EventoForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('lista_eventos')
+            return redirect('eventos_list')
     else:
         form = EventoForm()
-    return render(request, 'form_evento.html', {'form': form})
+    return render(request, 'evento_form.html', {'form': form})
 
 def editar_evento(request, id):
     evento = get_object_or_404(Evento, id=id)
@@ -56,14 +56,14 @@ def editar_evento(request, id):
         form = EventoForm(request.POST, instance=evento)
         if form.is_valid():
             form.save()
-            return redirect('lista_eventos')
+            return redirect('eventos_list')
     else:
         form = EventoForm(instance=evento)
-    return render(request, 'form_evento.html', {'form': form})
+    return render(request, 'evento_form.html', {'form': form})
 
 def eliminar_evento(request, id):
     evento = get_object_or_404(Evento, id=id)
     if request.method == 'POST':
         evento.delete()
-        return redirect('lista_eventos')
-    return render(request, 'confirmar_eliminar.html', {'evento': evento})
+        return redirect('eventos_list')
+    return render(request, 'evento_confirm_delete.html', {'evento': evento})
